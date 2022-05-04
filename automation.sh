ab2771351a3666afcb6702b63d98f0ec1606ce45
Gaurav Patil <40641173+gaurav06patil@users.noreply.github.com>
2022-05-04 22:09:35 +0530

    Add files via upload
    For Task 3

--- automation.sh
+++ automation.sh
@@ -34,4 +34,25 @@ then
   aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
  echo "File stored into the $s3_bucket bucket."
 
+ #Ensure that your script checks for the presence of the inventory.html file in /var/www/html/; if not found, creates it. This file will essentially serve as a web page to get the metadata of the archived logs
+cd /var/www/html/
+if [[ ! -f inventory.html ]]
+then
+sudo touch inventory.html
+fi
+
+echo -en "Log Type\t\t\t Date Created\t\t\t Type\t\t\t Size<br>" >> /var/www/html/inventory.html
+logsize=$(du -h /tmp/${myname}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+echo -en "apache2-logs\t\t ${timestamp}\t\t tar\t\t ${logsize}\t\t<br>" >> /var/www/html/inventory.html
+
+
+#script should create a cron job file in /etc/cron.d/ with the name 'automation' that runs the script /root/<git repository name>/automation.sh every day via the root user.
+CRON="/etc/cron.d/automation"
+if [ ! -f $CRON ]; then
+sudo touch $CRON
+/usr/bin/crontab $CRON
+/bin/echo "@daily /Automation_Project/automation.sh" >> sudo $CRON
+fi
+echo "End of script."
+
 #End of script.
